9a6932d251475cc190f14d7154f29c853fb9bd0e
Freshield <zxdsw199182@gmail.com>
2015-11-20 18:26:25 -0500

    FPGA INIT

---
+++ led3_module.v
@@ -0,0 +1,45 @@
+module led3_module
+(
+    CLK, RSTn, LED_Out
+);
+
+    input CLK;
+    input RSTn;
+    output LED_Out;
+    
+    /*************************************/
+    
+    parameter T1S = 26'd50_000_000;//Íâ²¿¾§Õñ50M
+    
+    /*************************************/
+    
+    reg [25:0]Count1;
+    
+    always @ ( posedge CLK or negedge RSTn )
+	    if( !RSTn )	
+	        Count1 <= 26'd0;
+	    else if( Count1 == T1S )
+	        Count1 <= 26'd0;
+	    else
+	        Count1 <= Count1 + 1'b1;
+	        
+	/*************************************/
+	
+	reg rLED_Out;
+	
+	always @ ( posedge CLK or negedge RSTn )
+        if( !RSTn ) 
+            rLED_Out <= 1'b0;
+        else if( Count1 >= 26'd37_5000_000 && Count1 < T1S)
+            rLED_Out <= 1'b1;
+        else 
+            rLED_Out <= 1'b0;
+            
+     /***************************************/
+     
+     assign LED_Out = rLED_Out;
+     
+     /***************************************/
+              
+    
+endmodule
---
+++ top_module.cdf
@@ -0,0 +1,13 @@
+/* Quartus II 64-Bit Version 11.0 Build 208 07/03/2011 Service Pack 1 SJ Full Version */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE6F17) Path("F:/AX301/TEST/DEMO_V/") File("top_module.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
