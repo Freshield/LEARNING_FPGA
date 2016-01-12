8091959707c9ae95e2dd72f45ad8058be42222c8
Freshield <zxdsw199182@gmail.com>
2016-01-12 13:59:56 -0500

    test1 flowled

---
+++ TEST1_FLOWLED/source/led0_module.v
@@ -0,0 +1,47 @@
+module led0_module
+(
+    CLK,RSTn,LED_Out
+);
+
+input CLK;
+input RSTn;
+output LED_Out;
+
+parameter T500MS = 25'd25_000_000;
+
+reg[24:0]Count1;
+
+always@(posedge CLK or negedge RSTn)
+if(!RSTn)
+    Count1<=25'd0;
+else if(Count1 == T500MS)
+    Count1<=25'd0;
+else
+    Count1<=Count1+1'b1;
+	
+	
+reg rLED_Out;
+
+always@(posedge CLK or negedge RSTn)
+if(!RSTn)
+    rLED_Out<=1'b0;
+else if(Count1>=25'd0 && Count1<25'd6_250_000)
+    rLED_Out<=1'b1;
+else
+    rLED_Out<=1'b0;
+	
+assign LED_Out=rLED_Out;
+
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ TEST1_FLOWLED/tcl/key_assignment.tcl
@@ -0,0 +1,8 @@
+set_location_assignment PIN_E1 -to CLK
+
+set_location_assignment PIN_E15 -to RSTn
+
+set_location_assignment PIN_G15 -to LED_Out[0]
+set_location_assignment	PIN_F16	-to LED_Out[1]
+set_location_assignment	PIN_F15	-to LED_Out[2]
+set_location_assignment	PIN_D16	-to LED_Out[3]
